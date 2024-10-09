86536723cf8979fc1a253c8875d0974ad47fd984
Aaron Rieder <737456+AARtwo@users.noreply.github.com>
2024-10-09 09:14:17 -0400

    Include BCR and F-Droid Priv

--- device-bluejay.mk
+++ device-bluejay.mk
@@ -129,6 +129,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.secure_element@1.2-service-gto
 
+# F-Droid
+PRODUCT_PACKAGES += \
+    F-DroidPrivilegedExtension
+# BCR
+PRODUCT_PACKAGES += \
+    BCR
+
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
 	frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
